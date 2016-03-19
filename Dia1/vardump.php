8663d0cca04dfb4e0414419758acb9cb459c4f60
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-03-19 09:43:19 -0600

    Vardump y break continue.php

---
+++ Dia1/vardump.php
@@ -0,0 +1,14 @@
+<?php 
+
+$texto = 'Alberto';
+$numero = 10;
+$numero2 = '5';
+$arreglo = array('Carlos', 'Cesar', 'Alejandro');
+$arreglo_asociativo = array('nombre' => 'Carlos', 'edad' => 20);
+$boleano = true;
+
+echo "<pre>";
+var_dump($arreglo_asociativo);
+echo "</pre>";
+
+?>

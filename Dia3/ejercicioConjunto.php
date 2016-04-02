7e741048e0a5017d355b8f5fdfa84f54328c4bd9
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-04-02 12:05:22 -0600

    Integración Bootstrap

--- Dia3/ejercicioConjunto.php
+++ Dia3/ejercicioConjunto.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 </head>
-
-
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <body>
 
@@ -79,7 +79,7 @@
 
 
 					
-					<table>
+					<table class="table table-striped">
 						<thead>
 							<tr>
 								<th>Vehiculo</th>

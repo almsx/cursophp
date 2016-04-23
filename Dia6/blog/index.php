e0a41937395d146c9b1c7de201ad659c62d48a99
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-04-23 13:51:47 -0500

    Blog Final

--- Dia6/blog/index.php
+++ Dia6/blog/index.php
@@ -9,7 +9,73 @@
 	<a href="admin.php">Ingresar al Admin de Blog</a>
 	<br/>
 	<br/>
-	Noticias
+	<h1>Noticias</h1>
+
+	<?php
+
+	 	include("../../utils/conexionDB.php");
+        
+        //$view_users_query="SELECT * from misAlumnos"; 
+	 	$view_users_query = "
+	 		SELECT 
+    		noticiasBlog.idNoticia,
+    		noticiasBlog.fechaPublicacion,
+    		noticiasBlog.tituloPublicacion,
+    		noticiasBlog.resumenPublicacion, 
+    		noticiasBlog.publicacionCompleta, 
+    		noticiasBlog.usuarioPublica,
+    		noticiasBlog.categoriaPublica,
+    		noticiasBlog.idNoticia,
+    		categoriasBlog.idCategoria,
+    		categoriasBlog.nombreCategoria,
+    		usuariosBlog.idUsuario, 
+    		usuariosBlog.nombres,
+    		usuariosBlog.pais,
+    		usuariosBlog.apPaterno,
+    		usuariosBlog.apMaterno
+
+			FROM noticiasBlog
+
+			LEFT JOIN usuariosBlog ON noticiasBlog.usuarioPublica = usuariosBlog.idUsuario 
+			LEFT JOIN categoriasBlog ON noticiasBlog.categoriaPublica = categoriasBlog.idCategoria
+
+			ORDER BY noticiasBlog.fechaPublicacion DESC
+
+			";
+        
+        	$run=mysqli_query($dbcon,$view_users_query);  
+  
+        	while($row=mysqli_fetch_array($run)) { 
+
+        		$autor = $row[11] . ' ' . $row[13] . '  ' . $row[14]; 
+
+        		?>
+
+        		<h1><?php echo $row[2]; ?></h1>
+        		<h3><?php echo "Publicado por ".$autor . " el Día " . $row[1]; ?></h3>
+        		<br/>
+        		<?php echo $row[4]; ?>
+        		<br/>
+        		<br/>
+        		<?php echo "Escrito en su computadora desde ".$row[12] . " para la Redacción de: " . $row[9] . " <a href='comentarios.php'> Comentarios</a>"; ?> 
+
+            	
+            	<!--
+            	//$nombre=$row[0];  
+            	//$paterno=$row[1];  
+            	//$materno=$row[2];  
+            	//$ano=$row[3];
+				//$mes=$row[4];
+            	//$dia=$row[5];
+
+            	-->
+            
+  
+  
+        	<?php } 
+
+
+	?>
 	
 </body>
 </html>
